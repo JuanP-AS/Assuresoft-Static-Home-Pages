12abaef3c3434702b0ccb41a8e1296dfcb28d16c
Juan Pablo Chavez Arteaga <juanpablo.chavez@assuresoft.com>
2024-07-22 16:16:04 -0400

    More responsive details

--- Bolivia/js/bolivia.js
+++ Bolivia/js/bolivia.js
@@ -3,9 +3,12 @@ const showSidebar = () => {
     document.getElementById("backdrop").classList.remove("hidden");
 }
 
-const hideSidebar = (e) => {
-    console.log(e?.target?.id)
+const hideSidebar = () => {
     document.getElementById("sidebar").style.left = "-100%";
     document.getElementById("backdrop").classList.add("hidden");
 }
 
+const scrollToId = (id) => {
+    document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
+    hideSidebar();
+}
